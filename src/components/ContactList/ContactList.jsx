acd474c84144fc9ce870d0ed51cd37de2ae5110d
MargaritaSukhova <margarytasu@gmail.com>
2023-06-19 17:24:21 +0300

    memoized selector

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,7 +1,7 @@
 import ContactListItem from '../ContactListItem/ContactListItem';
 import { ListOfContacts } from '../ContactList/ContactList.styled';
 import { useSelector } from 'react-redux';
-import { getContacts, getFilter } from 'redux/selectors';
+import { getFilter, getSortedContacts } from 'redux/selectors';
 
 const getVisibleContacts = (contacts, filter) => {
   const normalizedFilter = filter.toLowerCase();
@@ -13,7 +13,7 @@ const getVisibleContacts = (contacts, filter) => {
 };
 
 const ContactList = () => {
-  const contacts = useSelector(getContacts);
+  const contacts = useSelector(getSortedContacts);
   const filter = useSelector(getFilter);
 
   const filteredContacts = getVisibleContacts(contacts, filter);
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,10 +1,14 @@
-export const getContacts = state => {
-  const contactsCopy = [...state.contacts.items];
+import { createSelector } from '@reduxjs/toolkit';
+
+export const getContacts = state => state.contacts.items;
+
+export const getSortedContacts = createSelector([getContacts], contacts => {
+  const contactsCopy = [...contacts];
   const inAlphabeticalOrder = contactsCopy.sort((a, b) =>
     a.name.localeCompare(b.name)
   );
   return inAlphabeticalOrder;
-};
+});
 
 export const getIsLoading = state => state.contacts.isLoading;
 export const getError = state => state.contacts.error;
